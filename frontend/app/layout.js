37670618722853a09d76d9177bcf7c35fb446a64
novermohsen <novermohsen22@gmail.com>
2025-10-24 19:22:58 +0300

    add metadata

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -8,11 +8,62 @@ const cairo = Cairo({
   display: "swap",
 });
 export const metadata = {
-  title: "الصفحة الرئيسية لموقع كلية الهندسة جامعة سوهاج",
-  description: `مرحباً بكم في الموقع الرسمي لكلية الهندسة جامعة سوهاج حيث أنشئت الكلية بقرار الجمهورى رقم 324 لسنة 2007م وأنشئت اللائحة بقرار وزارى رقم 259 بتاريخ 9/2/2009 م وتمت الدراسة بها العام الدراسى 2008-2009 م وتم تخريج أول دفعة منها العام الدراسى 2012-2013 م.`,
+  title: {
+    default: "كلية الهندسة جامعة سوهاج - الموقع الرسمي",
+    template: "%s | كلية الهندسة جامعة سوهاج",
+  },
+  description:
+    "الموقع الرسمي لكلية الهندسة بجامعة سوهاج. اكتشف برامجنا الأكاديمية: الهندسة المدنية، الاتصالات والإلكترونيات، القوى الكهربائية، وهندسة الحاسبات. دليلك للقبول والأبحاث.",
   icons: {
     icon: "/icon/icon.png",
     apple: "/icon/icon.png",
+  },
+  keywords: [
+    "هندسة سوهاج",
+    "كلية الهندسة جامعة سوهاج",
+    "برامج الهندسة",
+    "قبول جامعة سوهاج",
+    "هندسة مدنية",
+    "هندسة حاسبات",
+    "جامعة سوهاج",
+  ],
+  authors: [
+    {
+      name: "كلية الهندسة جامعة سوهاج",
+      url: "https://sohag-faculty-of-engineering.vercel.app/",
+    },
+  ],
+  openGraph: {
+    title: "كلية الهندسة - جامعة سوهاج",
+    description:
+      "اكتشف البرامج الأكاديمية والبحثية المتميزة في قلب الصعيد. مستقبل الهندسة يبدأ هنا.",
+    url: "https://sohag-faculty-of-engineering.vercel.app/",
+    siteName: "كلية الهندسة جامعة سوهاج",
+    locale: "ar_AR",
+    type: "website",
+  },
+  twitter: {
+    site: "https://sohag-faculty-of-engineering.vercel.app/",
+    creator: "@SohagUnivEng",
+    title: "كلية الهندسة جامعة سوهاج",
+    description:
+      "الموقع الرسمي لـ كلية الهندسة بجامعة سوهاج. برامج مدنية، حاسبات، اتصالات، وقوى.",
+    images: "[رابط صورة شعار الكلية أو مبنى الكلية بدقة 1200x630 بكسل]",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
+  verification: {
+    google: "BFuqjbTvtG-3RjehxhKP1ZlUalh-wvdN--pXqvF1yY4",
+  },
+  alternates: {
+    canonical: "https://sohag-faculty-of-engineering.vercel.app/",
+    languages: {
+      "ar-EG": "https://sohag-faculty-of-engineering.vercel.app/",
+      "x-default": "https://sohag-faculty-of-engineering.vercel.app/",
+    },
   },
 };
 export default function RootLayout({ children }) {
