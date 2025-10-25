412f796147653949fbb2a37901160546eee6af84
novermohsen <novermohsen22@gmail.com>
2025-10-25 21:14:15 +0300

    fix robots.txt +1

--- frontend/app/robots.js
+++ frontend/app/robots.js
@@ -1,11 +1,9 @@
 export default function robots() {
-  const baseUrl = "sohag-faculty-of-engineering.vercel.app";
   return {
     rules: {
       userAgent: "*",
       allow: ["/", "/about-college", "/departments", "/news"],
       disallow: ["/private/"],
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
